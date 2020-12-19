997edc6a14886c9b59828212d72e4c19d27ee6d6
Grim <silencegrim@gmail.com>
2020-12-18 20:49:48 -0500

    Adds AssetImage

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,7 @@ void main() {
   runApp(App());
 }
 
-final imageURL =
-    'https://images.unsplash.com/photo-1608233528867-0e0a31a5ee53?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fA%3D%3D';
+final imageURL = 'assets/deadman.png';
 
 class App extends StatelessWidget {
   Widget build(BuildContext context) {
@@ -16,7 +15,7 @@ class App extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.grey[900],
         ),
-        body: Center(child: Image(image: NetworkImage(imageURL))),
+        body: Center(child: Image(image: AssetImage(imageURL))),
         floatingActionButton: FloatingActionButton(
           child: Text(
             "+",

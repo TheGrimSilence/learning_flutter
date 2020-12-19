1d0057884447a5cffd8638db6fffaee4ce5c9e28
Grim <silencegrim@gmail.com>
2020-12-18 20:44:28 -0500

    adds network image

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ void main() {
   runApp(App());
 }
 
+final imageURL =
+    'https://images.unsplash.com/photo-1608233528867-0e0a31a5ee53?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwzfHx8ZW58MHx8fA%3D%3D';
+
 class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,7 +16,7 @@ class App extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.grey[900],
         ),
-        body: Center(child: Column(children: <Widget>[Text("Hello there!")])),
+        body: Center(child: Image(image: NetworkImage(imageURL))),
         floatingActionButton: FloatingActionButton(
           child: Text(
             "+",
@@ -26,5 +29,3 @@ class App extends StatelessWidget {
     );
   }
 }
-
-
